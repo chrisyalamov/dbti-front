36ef2d7f3161ae2eaaf76e967aec1f0bc3fd6a39
Christian Yalamov <chrisyalamov@gmail.com>
2025-05-09 22:10:33 +0100

    trying with hash router due to gh pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { RouterProvider, createHashHistory, createRouter } from '@tanstack/react-router'
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
@@ -17,6 +17,7 @@ export const router = createRouter({
   scrollRestoration: true,
   defaultStructuralSharing: true,
   defaultPreloadStaleTime: 0,
+  history: createHashHistory()
 })
 
 // Register the router instance for type safety
